8d417342ca9b322fb4ff56e8e15605a20db3783e
Jean Barry <jfvbarry@gmail.com>
2022-03-07 22:20:37 -0600

    Add header

---
+++ public/style.js
@@ -0,0 +1,17 @@
+const logoGroup = document.querySelector('.navbar__logo-group');
+
+logoGroup.addEventListener('mouseenter', function() {
+    const navbarLogo = document.querySelector('.navbar__logo');
+    const navbarLogoText = document.querySelector('.navbar__logo-text');
+
+    navbarLogo.classList.add('navbar__logo--hover');
+    navbarLogoText.classList.add('navbar__logo-text--hover');
+});
+
+logoGroup.addEventListener('mouseleave', function() {
+    const navbarLogo = document.querySelector('.navbar__logo');
+    const navbarLogoText = document.querySelector('.navbar__logo-text');
+
+    navbarLogo.classList.remove('navbar__logo--hover');
+    navbarLogoText.classList.remove('navbar__logo-text--hover');
+});
